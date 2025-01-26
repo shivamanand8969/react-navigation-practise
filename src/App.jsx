c68925eaedc143c732599ac49ea99ec173543160
unknown <shivamanand8969@gmail.com>
2025-01-26 17:23:18 +0530

    push origin main

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import About from './screen/About'
 import Stack from './navigation/Stack'
 import Contact from './screen/Contact'
 import CustomDrawer from './component/CustomDrawer'
+import ProfileScreen from './screen/ProfileScreen'
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   const Drawer=createDrawerNavigator();
@@ -19,9 +21,34 @@ const App = () => {
 
         drawerContent={(props)=><CustomDrawer {...props}/>}
         >
-        <Drawer.Screen name="Home" component={Home}/>
-        <Drawer.Screen name="About" component={About}/>
-        <Drawer.Screen name="Contact" component={Contact}/>
+        <Drawer.Screen name="Home" component={Home} 
+          options={{
+            drawerIcon:({focused,size})=>(
+              <Icon name='home' size={size} color={focused?'blue':'black'}/>
+            )
+          }}
+        />
+        <Drawer.Screen name="About" component={About}
+          options={{
+            drawerIcon:({focused,size})=>(
+              <Icon name='infocirlceo' size={size} color={focused?'blue':'black'}/>
+            )
+          }}
+        />
+        <Drawer.Screen name="Contact" component={Contact}
+          options={{
+            drawerIcon:({focused,size})=>(
+              <Icon name='contacts' size={20} color={focused? 'blue':'black'}/>
+            )
+          }}
+        />
+        <Drawer.Screen name="Profile" component={ProfileScreen}
+          options={{
+            drawerIcon:({focused,size})=>(
+              <Icon name='profile' size={size} color={focused? 'blue':'black'}/>
+            )
+          }}
+        />
        </Drawer.Navigator>
     </NavigationContainer>
   )
--- src/component/CustomDrawer.jsx
+++ src/component/CustomDrawer.jsx
@@ -1,17 +1,29 @@
-import { View, Text, Linking } from 'react-native'
-import React from 'react'
-import { DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer'
-import About from '../screen/About'
 
+import { View, Text, ImageBackground, Image } from 'react-native'
+import React from 'react'
+import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer'
+import Icon from 'react-native-vector-icons/FontAwesome5';
 const CustomDrawer = (props) => {
   return (
-   <DrawerContentScrollView {...props}>
-    <Text>Custom Drawer</Text>
-        <DrawerItemList {...props}
-         
-        />
-        {/* <DrawerItem label={()=><About/>} onPress={()=>Linking.openURL('https://google.com/')}/> */}
-   </DrawerContentScrollView>
+    <View style={{ flex: 1 }}>
+      <DrawerContentScrollView {...props} contentContainerStyle={{ backgroundColor: '#8200d6' }}>
+        <ImageBackground source={{ uri: 'https://w0.peakpx.com/wallpaper/222/995/HD-wallpaper-color-palette-abstract-resolution-abstract-and-background-den.jpg' }} style={{ padding: 20, borderRadius: 10 }}>
+          <Image source={{ uri: "https://cdn3.vectorstock.com/i/1000x1000/30/97/flat-business-man-user-profile-avatar-icon-vector-4333097.jpg" }} style={{ height: 80, width: 80, borderRadius: 10, marginBottom: 10 }} />
+          <Text style={{ fontSize: 20, color: 'white', fontFamily: 'Roboto-Regular' }}>Shivam Anand</Text>
+          <View style={{ flexDirection: 'row', gap: 2, alignItems: 'center' }}>
+            <Text style={{ fontSize: 20, fontWeight: 'bold' }}> 100</Text>
+            <Icon name="coins" size={15} color="white" />
+          </View>
+        </ImageBackground>
+        
+        <View style={{flex:1,backgroundColor:'white',paddingTop:10}}>
+          <DrawerItemList {...props} />
+        </View>
+      </DrawerContentScrollView>
+      <View>
+        <Text>Our Custom Text</Text>
+      </View>
+    </View>
   )
 }
 
